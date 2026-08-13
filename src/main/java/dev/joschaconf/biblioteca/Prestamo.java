114c5f13a8cc3438853ddbd8895f654f9010be84
José Ignacio Chacón <jos.chaconf@duocuc.cl>
2026-08-12 23:13:36 -0400

    Primer commit de Prestamo.java

--- src/main/java/dev/joschaconf/biblioteca/Prestamo.java
+++ src/main/java/dev/joschaconf/biblioteca/Prestamo.java
@@ -1,4 +1,31 @@
 package dev.joschaconf.biblioteca;
 
 public class Prestamo {
+    private Libro libro;
+    private String estudiante;
+    private String fechaPrestamo;
+    private String fechaDevolucion;
+    private boolean devuelto;
+
+    public Prestamo(Libro libro, String estudiante, String fechaPrestamo, String fechaDevolucion) {
+        this.libro = libro;
+        this.estudiante = estudiante;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+        this.devuelto = false;
+    }
+
+    public void registrarDevolucion() {
+        this.devuelto = true;
+        this.libro.devolver(); // avisa al libro que vuelve a estar disponible
+    }
+
+    public boolean estaVencido(String fechaActual) {
+        return !devuelto && fechaActual.compareTo(fechaDevolucion) > 0;
+    }
+
+    public void mostrarDetalle() {
+        System.out.println("Estudiante: " + estudiante + " | Préstamo: " + fechaPrestamo
+                + " | Devolución: " + fechaDevolucion + " | Devuelto: " + devuelto);
+    }
 }
